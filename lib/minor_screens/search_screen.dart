d968722e1e4041a2787ce90f0028dcc1cb9e4abc
ynanaro_morexex <ynanaromorexex@gmail.com>
2023-05-03 23:55:47 +0300

    search engine

--- lib/minor_screens/search_screen.dart
+++ lib/minor_screens/search_screen.dart
@@ -1,25 +1,171 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:multi_store_app/minor_screens/product_details.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
 
   @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  String searchInput = '';
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade300,
       appBar: AppBar(
         elevation: 0,
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.grey.shade300,
         leading: IconButton(
           icon: const Icon(
             Icons.arrow_back_ios_new,
-            color: Colors.black,
+            color: Color.fromARGB(255, 8, 7, 7),
           ),
           onPressed: () {
             Navigator.pop(context);
           },
         ),
-        title: const CupertinoSearchTextField(),
+        title: CupertinoSearchTextField(
+          autofocus: true,
+          backgroundColor: Colors.white,
+          onChanged: (value) {
+            setState(() {
+              searchInput = value;
+            });
+          },
+        ),
+      ),
+      body: searchInput == ''
+          ? Center(
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.purple,
+                    borderRadius: BorderRadius.circular(25)),
+                height: 30,
+                width: MediaQuery.of(context).size.width * 0.7,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: const [
+                    Icon(
+                      Icons.search,
+                      color: Colors.grey,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 15,
+                      ),
+                      child: Text(
+                        'Search For Anything',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          : StreamBuilder<QuerySnapshot>(
+              stream:
+                  FirebaseFirestore.instance.collection('products').snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Material(
+                    child: Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                }
+
+                final result = snapshot.data!.docs.where(
+                  (e) => e['proname'.toLowerCase()]
+                      .contains(searchInput.toLowerCase()),
+                );
+
+                return ListView(
+                  children: result.map((e) => SearchModel(e: e)).toList(),
+                );
+              },
+            ),
+    );
+  }
+}
+
+class SearchModel extends StatelessWidget {
+  final dynamic e;
+  const SearchModel({super.key, this.e});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => ProductDetailsScreen(
+                      proList: e,
+                    )));
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          height: 100,
+          width: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.only(
+              right: 10,
+            ),
+            child: Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: SizedBox(
+                    height: 100,
+                    width: 100,
+                    child: Image(
+                      image: NetworkImage(e['proimages'][0]),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Flexible(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        e['proname'],
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        style: const TextStyle(
+                            fontSize: 14,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      Text(
+                        e['prodesc'],
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                        style: const TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
